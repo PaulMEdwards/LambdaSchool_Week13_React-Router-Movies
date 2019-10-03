48907137194c540b4a1477a287354b227ad42460
Paul M Edwards <pauledwards@gmail.com>
2019-10-02 20:51:31 -0700

    MVP revised.

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,6 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
       <Route exact path="/" component={MovieList} />
       <Route exact path="/movies/:id" component={Movie} />
     </div>
